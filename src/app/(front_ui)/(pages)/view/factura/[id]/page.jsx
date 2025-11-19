9c68967a423385a289922607f120898115fe5800
nmmartinezp <nikimartin56@gmail.com>
2025-11-19 19:29:02 -0400

    actualizado - fetch de factura por id

--- src/app/(front_ui)/(pages)/view/factura/[id]/page.jsx
+++ src/app/(front_ui)/(pages)/view/factura/[id]/page.jsx
@@ -3,65 +3,34 @@ import { Form, Input, Button, DateInput, Divider } from "@heroui/react";
 import { parseDate } from "@internationalized/date";
 import Link from "next/link";
 import { User, Calendar, Box, Coins, ChevronLeft } from "lucide-react";
+import { useState, useEffect } from "react";
+import { redirect } from "next/dist/server/api-utils";
 
-const dataInvoice = {
-  id: 2,
-  cliente: "María Gómez",
-  fecha: "2025-01-11",
-  total: 480.4,
-  detalles: [
-    {
-      id: 10,
-      producto_id: 3,
-      nombre_producto: "Monitor LED 24”",
-      precio: 180.1,
-      cantidad: 1,
-      subtotal: 180.1,
-    },
-    {
-      id: 11,
-      producto_id: 8,
-      nombre_producto: "Teclado mecánico",
-      precio: 100.1,
-      cantidad: 3,
-      subtotal: 300.3,
-    },
-    {
-      id: 12,
-      producto_id: 8,
-      nombre_producto: "Teclado mecánico",
-      precio: 100.1,
-      cantidad: 3,
-      subtotal: 300.3,
-    },
-    {
-      id: 13,
-      producto_id: 8,
-      nombre_producto: "Teclado mecánico",
-      precio: 100.1,
-      cantidad: 3,
-      subtotal: 300.3,
-    },
-    {
-      id: 14,
-      producto_id: 8,
-      nombre_producto: "Teclado mecánico",
-      precio: 100.1,
-      cantidad: 3,
-      subtotal: 300.3,
-    },
-    {
-      id: 15,
-      producto_id: 8,
-      nombre_producto: "Teclado mecánico",
-      precio: 100.1,
-      cantidad: 3,
-      subtotal: 300.3,
-    },
-  ],
-};
+function Factura({ params }) {
+  const [facuraData, setFactura] = useState({});
 
-function Factura() {
+  useEffect(() => {
+    const fetchFactura = async () => {
+      try {
+        const { id } = await params;
+        const response = await fetch(`/api/v1/facturas/${id}`);
+        if (!response.ok) {
+          redirect("/");
+        }
+        const result = await response.json();
+        if (result.error === "false") {
+          setFactura((prev) => ({ ...prev, ...result.data }));
+        } else {
+          throw new Error(result.mensaje || "Formato de datos incorrecto");
+        }
+      } catch (err) {
+        redirect("/");
+      }
+    };
+
+    fetchFactura();
+  }, []);
+
   return (
     <div className="w-full h-full flex items-center justify-center p-6 md:p-0">
       <Form className="w-full h-full gap-14 md:gap-0 flex flex-col">
@@ -76,7 +45,7 @@ function Factura() {
               labelPlacement="outside"
               name="cliente"
               placeholder="Nombre del cliente"
-              value={dataInvoice.cliente}
+              value={facuraData.cliente ? facuraData.cliente : ""}
               startContent={<User />}
               type="text"
             />
@@ -87,7 +56,11 @@ function Factura() {
               label="Fecha de Emisión"
               labelPlacement={"outside"}
               startContent={<Calendar />}
-              value={parseDate(dataInvoice.fecha)}
+              value={
+                facuraData.fecha
+                  ? parseDate(facuraData.fecha?.split("T")[0])
+                  : undefined
+              }
             />
             <Input
               isReadOnly
@@ -95,66 +68,72 @@ function Factura() {
               label="Total"
               labelPlacement="outside"
               placeholder="0.00"
-              value={dataInvoice.total}
+              value={facuraData.total ? facuraData.total : 0}
               type="number"
             />
+            {facuraData && (
+              <div className="text-small text-default-500">
+                You submitted: <code>{JSON.stringify(facuraData)}</code>
+              </div>
+            )}
           </div>
           <div className="flex-1 h-full flex flex-col gap-2">
             <div className="sticky w-full bottom-0 top-0 flex items-center justify-center bg-foreground-50 rounded p-2 font-bold">
               Detalles de Factura
             </div>
             <div className="flex flex-col max-h-[60dvh] md:h-full overflow-y-scroll scrollbar-hide gap-4 z-10">
-              {dataInvoice.detalles.map((detail) => (
-                <div key={detail.id} className="w-full flex flex-col gap-1">
-                  <div
-                    className="grid grid-cols-12 md:flex justify-evenly w-full gap-2"
-                    key={detail.id}
-                  >
-                    <Input
-                      isReadOnly
-                      label="Producto"
-                      className="max-w-xs col-span-12"
-                      labelPlacement="outside"
-                      name="producto"
-                      placeholder="Nombre del producto"
-                      value={detail.nombre_producto}
-                      startContent={<Box />}
-                      type="text"
-                    />
-                    <Input
-                      isReadOnly
-                      className="max-w-24 col-span-4"
-                      startContent={<Coins />}
-                      label="P. Unitario"
-                      labelPlacement="outside"
-                      placeholder="0.00"
-                      value={detail.precio}
-                      type="number"
-                    />
-                    <Input
-                      isReadOnly
-                      className="max-w-24 col-span-4"
-                      startContent={<Coins />}
-                      label="Cantidad"
-                      labelPlacement="outside"
-                      placeholder="0.00"
-                      value={detail.cantidad}
-                      type="number"
-                    />
-                    <Input
-                      isReadOnly
-                      className="max-w-24 col-span-4"
-                      startContent={<Coins />}
-                      label="Subtotal"
-                      labelPlacement="outside"
-                      placeholder="0.00"
-                      value={detail.subtotal}
-                      type="number"
-                    />
+              {facuraData.detalles &&
+                facuraData.detalles.map((detail) => (
+                  <div key={detail.id} className="w-full flex flex-col gap-1">
+                    <div
+                      className="grid grid-cols-12 md:flex justify-evenly w-full gap-2"
+                      key={detail.id}
+                    >
+                      <Input
+                        isReadOnly
+                        label="Producto"
+                        className="max-w-xs col-span-12"
+                        labelPlacement="outside"
+                        name="producto"
+                        placeholder="Nombre del producto"
+                        value={detail.nombre_producto}
+                        startContent={<Box />}
+                        type="text"
+                      />
+                      <Input
+                        isReadOnly
+                        className="max-w-24 col-span-4"
+                        startContent={<Coins />}
+                        label="P. Unitario"
+                        labelPlacement="outside"
+                        placeholder="0.00"
+                        value={detail.precio}
+                        type="number"
+                      />
+                      <Input
+                        isReadOnly
+                        className="max-w-24 col-span-4"
+                        startContent={<Coins />}
+                        label="Cantidad"
+                        labelPlacement="outside"
+                        placeholder="0.00"
+                        value={detail.cantidad}
+                        type="number"
+                      />
+                      <Input
+                        isReadOnly
+                        className="max-w-24 col-span-4"
+                        startContent={<Coins />}
+                        label="Subtotal"
+                        labelPlacement="outside"
+                        placeholder="0.00"
+                        value={detail.subtotal}
+                        type="number"
+                      />
+                    </div>
+                    <Divider key={`dv${detail.id}`} className="my-1" />
                   </div>
-                  <Divider key={`dv${detail.id}`} className="my-1" />
-                </div>
-              ))}
+                ))}
             </div>
             <div className="flex sticky bottom-0 top-full w-full justify-center items-center bg-foreground-50 rounded p-2"></div>
           </div>
