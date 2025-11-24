d7ff360e4a3e901167ba2579d13df2fdddadc248
nmmartinezp <nikimartin56@gmail.com>
2025-11-24 14:48:05 -0400

    fix - ssl condition

--- src/app/(back_api)/lib/db.js
+++ src/app/(back_api)/lib/db.js
@@ -2,9 +2,8 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DB_URI,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl:
+    process.env.PRODUCTION === "true" ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
