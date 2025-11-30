ea6b6139505d1aa7b74f4cb9e8e202931f425111
nmmartinezp <nikimartin56@gmail.com>
2025-11-30 19:39:18 -0400

    tag - v.final

--- src/app/(front_ui)/(pages)/(home)/page.jsx
+++ src/app/(front_ui)/(pages)/(home)/page.jsx
@@ -10,9 +10,7 @@ export default function Home() {
           <h1 className="text-2xl md:text-4xl font-bold">
             Facturación de Negocio
           </h1>
-          <h2 className="md:text-xl opacity-85">
-            UI Frontend Probando cambios 2
-          </h2>
+          <h2 className="md:text-xl opacity-85">UI Frontend</h2>
         </header>
         <main className="col-span-12 flex items-center justify-center">
           <div className="w-full gap-6 grid grid-cols-12 px-2">
