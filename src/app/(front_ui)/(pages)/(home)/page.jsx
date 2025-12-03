8ee8f32bdf1c7b76de82985c1f0032b762fa1b6f
nmmartinezp <nikimartin56@gmail.com>
2025-12-02 23:03:22 -0400

    test - prueba

--- src/app/(front_ui)/(pages)/(home)/page.jsx
+++ src/app/(front_ui)/(pages)/(home)/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
           <h1 className="text-2xl md:text-4xl font-bold">
             Facturación de Negocio
           </h1>
-          <h2 className="md:text-xl opacity-85">Frontend UI demostracion</h2>
+          <h2 className="md:text-xl opacity-85">Frontend UI</h2>
         </header>
         <main className="col-span-12 flex items-center justify-center">
           <div className="w-full gap-6 grid grid-cols-12 px-2">
@@ -30,29 +30,6 @@ export default function Home() {
                       alt="Card background"
                       className="z-0 w-full h-full object-contain"
                       src="/module/home/assets/icons/ef.png"
-                      width={500}
-                      height={500}
-                    />
-                  </div>
-                </div>
-              </Link>
-            </Card>
-            <Card className="col-span-12 md:col-span-6 h-[200px] md:h-[300px] hover:scale-[97%] transition-transform duration-300 active:scale-90 cursor-pointer">
-              <Link href="/view/facturas" className="w-full h-full">
-                <CardHeader className="absolute z-10 top-1 flex-col gap-2 items-start!">
-                  <p className="md:text-lg text-white/70 uppercase font-medium bg-gray-950/50 px-4 rounded">
-                    Listado de facturas emitidas
-                  </p>
-                  <h3 className="text-white font-bold text-lg md:text-xl bg-gray-950/50 px-4 rounded">
-                    Ver Facturas
-                  </h3>
-                </CardHeader>
-                <div className="w-full h-full flex items-center justify-center">
-                  <div className="h-2/3">
-                    <Image
-                      alt="Card background"
-                      className="z-0 w-full h-full object-contain"
-                      src="/module/home/assets/icons/lf.png"
                       width={500}
                       height={500}
                     />
